3da0ea6981a9f0347f53560d33f89edee4204a48
davidmonterocrespo24 <davidmonterocrespo24@gmial.com>
2023-10-08 11:48:03 -0300

    [MIG] muk_web_client_refresh: Migration to 14.0

--- muk_web_client_refresh/__manifest__.py
+++ muk_web_client_refresh/__manifest__.py
@@ -23,7 +23,7 @@
 {
     "name": "MuK Web Refresh",
     "summary": """Web Client Refresh""",
-    "version": "12.0.3.0.5",
+    "version": "14.0.1.0.0",
     "category": "Extra Tools",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
